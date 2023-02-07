95a8bd26f728d06b9fd89f260d3e2f4a4d215295
stallionblade <119752878+stallionblade@users.noreply.github.com>
2023-02-07 12:59:50 -0500

    Create module for load page

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 // alert('Hello')
 
 import './style.css';
+import './loadpage'
+
+// const content = document.querySelector('#content')
+
+// const header = document.createElement('h2')
+// const copy = document.createElement('div')
+
+// header.textContent = "A Bakery!!!"
+// copy.textContent = "This bakery is an inviting and cozy environment where customers can enjoy a variety of freshly-baked breads, pastries, and other delicious treats. With a range of tempting options, such as croissants, éclairs, macarons, tarts, and more, there’s something for everyone. All of the products are crafted with the highest quality ingredients, and the inviting atmosphere and attentive service make it the perfect place to grab a quick bite or linger over a cup of coffee."
+
+// content.appendChild(header)
+// content.appendChild(copy)
---
+++ src/loadpage.js
@@ -0,0 +1,10 @@
+const content = document.querySelector('#content')
+
+const header = document.createElement('h2')
+const copy = document.createElement('div')
+
+header.textContent = "A Bakery!???!! wow" 
+copy.textContent = "This bakery is an inviting and cozy environment where customers can enjoy a variety of freshly-baked breads, pastries, and other delicious treats. With a range of tempting options, such as croissants, éclairs, macarons, tarts, and more, there’s something for everyone. All of the products are crafted with the highest quality ingredients, and the inviting atmosphere and attentive service make it the perfect place to grab a quick bite or linger over a cup of coffee."
+
+content.appendChild(header)
+content.appendChild(copy)
