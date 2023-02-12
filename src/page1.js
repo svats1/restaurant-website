52ea54990cd23758e1291362b85654cc04ca630b
stallionblade <119752878+stallionblade@users.noreply.github.com>
2023-02-11 20:28:51 -0500

    Use assets with webpack

--- src/page1.js
+++ src/page1.js
@@ -1,43 +1,17 @@
-// import { header, page, container, item1, item2, item3 } from "./pageItems";
 import { header, page, container, Item } from "./pageItems";
+import image1 from "../media/country-loaf.jpeg";
+import image2 from "../media/milk-toast.jpeg";
+import image3 from "../media/sourdough.jpeg";
 
 export default function breads() {
-  container.innerHTML = "";
   page.innerHTML = "";
+  container.innerHTML = "";
 
-  header.textContent = "Breads!";
+  header.textContent = "Breads";
 
-  // const item1 = createItem(
-  //   "/Users/svats/repos/restaurant-website/media/country-loaf.jpeg",
-  //   "country loaf",
-  //   "your average bread"
-  // );
-  // const item2 = createItem(
-  //   "/Users/svats/repos/restaurant-website/media/milk-toast.jpeg",
-  //   "milk toast",
-  //   "kiddie bread"
-  // );
-  // const item3 = createItem(
-  //   "/Users/svats/repos/restaurant-website/media/sourdough.jpeg",
-  //   "sourdough",
-  //   "real bread"
-  // );
-
-  const item1 = new Item(
-    "/Users/svats/repos/restaurant-website/media/country-loaf.jpeg",
-    "country loaf",
-    "your average bread"
-  );
-  const item2 = new Item(
-    "/Users/svats/repos/restaurant-website/media/milk-toast.jpeg",
-    "milk toast",
-    "kiddie bread"
-  );
-  const item3 = new Item(
-    "/Users/svats/repos/restaurant-website/media/sourdough.jpeg",
-    "sourdough",
-    "real bread"
-  );
+  const item1 = new Item(`${image1}`, "country loaf", "your average bread");
+  const item2 = new Item(`${image2}`, "milk toast", "kiddie bread");
+  const item3 = new Item(`${image3}`, "sourdough", "real bread");
 
   page.appendChild(header);
   page.appendChild(container);
--- src/pageItems.js
+++ src/pageItems.js
@@ -2,31 +2,9 @@ export const page = document.createElement("section");
 export const header = document.createElement("h1");
 export const container = document.createElement("div");
 
-// export function createItem(picture, title, desc) {
-//   const item = document.createElement("div");
-//   const itemPicture = document.createElement("div");
-//   const itemTitle = document.createElement("div");
-//   const itemDesc = document.createElement("div");
-
-//   item.className = "item";
-//   itemPicture.className = "item-picture";
-//   itemTitle.className = "item-title";
-//   itemDesc.className = "item-desc";
-
-//   itemPicture.style.backgroundImage = `url(${picture})`;
-//   itemTitle.textContent = title;
-//   itemDesc.textContent = desc;
-
-//   item.appendChild(itemPicture);
-//   item.appendChild(itemTitle);
-//   item.appendChild(itemDesc);
-
-//   container.appendChild(item);
-// }
-
 export class Item {
   constructor(picture, title, desc) {
-    (this.picture = picture), (this.title = title), (this.desc = desc);
+    // (this.picture = picture), (this.title = title), (this.desc = desc);
 
     const item = document.createElement("div");
     const itemPicture = document.createElement("div");
@@ -50,39 +28,10 @@ export class Item {
   }
 }
 
-// const item1 = new Item();
-
-// const item1 = document.createElement("div");
-// const item2 = document.createElement("div");
-// const item3 = document.createElement("div");
-
-// const itemList = [item1, item2, item3];
-
-// // Each item has a picture, description, and title
-// itemList.forEach((element) => {
-//   const itemPicture = document.createElement("div");
-//   const itemTitle = document.createElement("div");
-//   const itemDesc = document.createElement("div");
-//   itemPicture.className = "item-picture";
-//   itemTitle.className = "item-title";
-//   itemDesc.className = "item-desc";
-//   element.appendChild(itemPicture);
-//   element.appendChild(itemTitle);
-//   element.appendChild(itemDesc);
-// });
-
-// export { item1, item2, item3 };
-
 // Set class names
 page.className = "page";
 header.className = "heading";
 container.className = "container";
-// item1.className = item2.className = item3.className = "item";
-
-// Add items to container
-// container.appendChild(item1);
-// container.appendChild(item2);
-// container.appendChild(item3);
 
 // Add elements to page
 page.appendChild(header);
