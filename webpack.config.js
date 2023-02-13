7e9d2b9725d305eb042233f7945c1fa4a037b8ba
stallionblade <119752878+stallionblade@users.noreply.github.com>
2023-02-13 12:55:10 -0500

    Fix item height overflow

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,6 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
-  devtool: "source-map",
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Bakery Icon",
-      favicon: "./media/icons8-bakery.png",
-    }),
-  ],
   entry: "./src/index.js",
   output: {
     filename: "main.js",
